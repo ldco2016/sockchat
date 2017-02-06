f5b8617618923997000e644c3e69150245ed489f
danale <danale@danales-MacBook-Pro.local>
2017-02-06 10:53:57 -0500

    Added Messaging function

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -6,9 +6,23 @@ class MessageForm extends React.Component {
 	render(){
 		return(
 			<div>
-				MESSAGEFORM
+				<form onSubmit={this.onSubmit.bind(this)}>
+					<input type="text" className="form-control" ref="text" placeholder="Please type a message..." />
+				</form>
 			</div>
 		)
+	}
+
+	onSubmit(e){
+		e.preventDefault();
+
+		this.props.emit('messageAdded', {
+			timeStamp: Date.now(),
+			text: this.refs.text.value.trim()
+		});
+
+		// Clear form
+		this.refs.text.value = '';
 	}
 }
 
